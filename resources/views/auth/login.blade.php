14a18addd4dfc6b9fa38d552dfa618ea6f16eb01
edobarto <edo99.barto@gmail.com>
2023-02-14 15:11:32 +0100

    feat: aggiunta lingua italiana

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,11 +33,15 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+{{--                @if (Route::has('password.request'))--}}
+{{--                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
+{{--                        {{ __('Forgot your password?') }}--}}
+{{--                    </a>--}}
+{{--                @endif--}}
+
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                                        Registrati
+                                    </a>
 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
