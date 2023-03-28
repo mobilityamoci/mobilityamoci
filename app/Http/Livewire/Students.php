30966db933d66ea480577316f368e93bfd913d28
edobarto <edo99.barto@gmail.com>
2023-03-28 11:51:02 +0200

    fix

--- app/Http/Livewire/Students.php
+++ app/Http/Livewire/Students.php
@@ -115,7 +115,7 @@ class Students extends Component
 
 
                 if ($item->trips->isNotEmpty()) {
-                    $string = '1) Da <b>' . $this->comuni[$item->town_istat]['comune'] . ', '.$item->address.'</b> in ';
+                    $string = '1) Da <b>' . $this->comuni[$item->town_istat]['comune'] . ' ('.$item->address.')</b> in ';
                     $i = 0;
 
                     foreach ($item->trips as $trip) {
@@ -125,7 +125,7 @@ class Students extends Component
                         $string .= '<b>' . $trip->transport1->name . '</b>';
                         $trip->transport2 ? $string .= '<b>/' . $trip->transport2->name . '</b>' : $string .= '';
                         if ($trip->town_istat)
-                            $string .= ' fino a <b>' . $this->comuni[$trip->town_istat]['comune'] . ', '.$trip->address.'</b>';
+                            $string .= ' fino a <b>' . $this->comuni[$trip->town_istat]['comune'] . ' ('.$trip->address.')</b>';
                         else
                             $string .= ' (comune assente)';
                         $i++;
