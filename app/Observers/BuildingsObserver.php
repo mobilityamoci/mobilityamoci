65a5922d3bb3330c20a10f5d026f8e2129105607
edobarto <edo99.barto@gmail.com>
2023-10-25 18:09:12 +0200

    georef buildings

---
+++ app/Observers/BuildingsObserver.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Observers;
+
+use App\Interfaces\IQgisService;
+use App\Models\Building;
+use App\Services\QgisService;
+
+class BuildingsObserver
+{
+
+    private IQgisService $qgisService;
+
+    public function __construct(IQgisService $qgisService)
+    {
+        $this->qgisService = $qgisService;
+    }
+
+    /**
+     * Handle the Building "created" event.
+     *
+     * @param Building $building
+     * @return void
+     */
+    public function creating(Building $building)
+    {
+        $building = $this->qgisService::georefBuilding($building);
+    }
+
+    /**
+     * Handle the Building "updated" event.
+     *
+     * @param Building $building
+     * @return void
+     */
+    public function updating(Building $building)
+    {
+        if ($building->isDirty(['address', 'town_istat'])) {
+            $building = $this->qgisService::georefBuilding($building);
+        }
+    }
+
+    /**
+     * Handle the Building "deleted" event.
+     *
+     * @param Building $building
+     * @return void
+     */
+    public function deleted(Building $building)
+    {
+        //
+    }
+
+    /**
+     * Handle the Building "restored" event.
+     *
+     * @param Building $building
+     * @return void
+     */
+    public function restored(Building $building)
+    {
+        //
+    }
+
+    /**
+     * Handle the Building "force deleted" event.
+     *
+     * @param Building $building
+     * @return void
+     */
+    public function forceDeleted(Building $building)
+    {
+        //
+    }
+}
