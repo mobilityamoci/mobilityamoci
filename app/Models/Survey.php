c936e17f5f077146c9d182fa6391215c90694395
edobarto <edo99.barto@gmail.com>
2024-04-26 16:47:32 +0200

    conteggio risposte sondaggio

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -2,23 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Staudenmeir\EloquentHasManyDeep\HasRelationships;
 
 class Survey extends \MattDaneshvar\Survey\Models\Survey
 {
+
+    use HasRelationships;
     protected $fillable = ['school_id', 'settings', 'name', 'uuid'];
 
     public static function boot()
     {
         parent::boot();
         self::creating(function ($model) {
-            $model->uuid = (string) Str::uuid();
+            $model->uuid = (string)Str::uuid();
         });
     }
+
     public function mySections()
     {
         return $this->morphedByMany(Section::class, 'surveyable')->withTimestamps();
+    }
+
+    public function students()
+    {
+        return $this->hasManyDeepFromRelations($this->mySections(), (new Section)->students());
     }
 }
