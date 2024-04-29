754331dcec164a6dbc394cc03c595ec7c279549f
edobarto <edo99.barto@gmail.com>
2024-04-29 16:48:13 +0200

    fix

--- resources/views/livewire/surveys-student.blade.php
+++ resources/views/livewire/surveys-student.blade.php
@@ -1,6 +1,5 @@
 <div class="md:flex justify-center bg-white bg-opacity-75">
     <div class="m-10 md:w-3/4 space-y-8">
-        {!! $component !!}
         @livewire($component, ['surveyId' => $selectedSurveyId], key(random_int(-999,999)) )
     </div>
 </div>
--- resources/views/livewire/surveys.blade.php
+++ resources/views/livewire/surveys.blade.php
@@ -1,4 +1,3 @@
 <div x-data="{}" style="width: 100%; height: 90vh">
-        {!! $component !!}
-        @livewire($component, ['surveyId' => $selectedSurveyId], key(random_int(-999,999)) )
+    @livewire($component, ['surveyId' => $selectedSurveyId], key(random_int(-999,999)) )
 </div>
