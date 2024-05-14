9ab24804a5da02186b7e988d8ae5c4533cc35795
edobarto <edo99.barto@gmail.com>
2024-05-14 15:50:44 +0200

    fix

--- resources/views/livewire/show-graphs.blade.php
+++ resources/views/livewire/show-graphs.blade.php
@@ -3,7 +3,7 @@
     <x-select class="col-auto" wire:model="selectedSchoolId" wire:change="refresh" label="Seleziona Scuola"
               id="school">
         @canAny(['admin','all_schools'])
-        <option @selected(!$selectedSchoolId) value="0">Tutte le scuole</option>
+            <option @selected(!$selectedSchoolId) value="0">Tutte le scuole</option>
         @endcanAny
         @foreach($schools as $school)
             <option @selected($selectedSchoolId == $school->id) value="{{$school->id}}">{{$school->name}}</option>
@@ -40,35 +40,39 @@
                             <div class="w-1/2 p-8">
                                 <table class="my-table table-auto p-5">
                                     <tbody>
-                                    <tr>
-                                        <th class="my-th">Carburante consumato in un anno</th>
-                                        <th class="my-th">{{$this->pollutionArray['carburante']}} lt</th>
-                                    </tr>
-                                    <tr>
-                                        <th class="my-th">CO2 emesso in un anno</th>
-                                        <th class="my-th">{{$this->pollutionArray['co2']}} kg</th>
-                                    </tr>
-                                    <tr>
-                                        <th class="my-th">CO emesso in un anno</th>
-                                        <th class="my-th">{{$this->pollutionArray['co']}} kg</th>
-                                    </tr>
-                                    <tr>
-                                        <th class="my-th">CO emesso in un anno</th>
-                                        <th class="my-th">{{$this->pollutionArray['co']}} kg</th>
-                                    </tr>
-                                    <tr>
-                                        <th class="my-th">NOX emesso in un anno</th>
-                                        <th class="my-th">{{$this->pollutionArray['nox']}} kg</th>
-                                    </tr>
-                                    <tr>
-                                        <th class="my-th">PM10 emesse in un anno</th>
-                                        <th class="my-th">{{$this->pollutionArray['pm10']}} kg</th>
-                                    </tr>
-                                    <tr>
-                                        <th class="my-th">Alberi necessari per neutralizzare la CO2 emessa annualmente
-                                        </th>
-                                        <th class="my-th">{{$this->pollutionArray['trees']}} alberi</th>
-                                    </tr>
+                                    @if($this->pollutionArray)
+
+                                        <tr>
+                                            <th class="my-th">Carburante consumato in un anno</th>
+                                            <th class="my-th">{{$this->pollutionArray['carburante']}} lt</th>
+                                        </tr>
+                                        <tr>
+                                            <th class="my-th">CO2 emesso in un anno</th>
+                                            <th class="my-th">{{$this->pollutionArray['co2']}} kg</th>
+                                        </tr>
+                                        <tr>
+                                            <th class="my-th">CO emesso in un anno</th>
+                                            <th class="my-th">{{$this->pollutionArray['co']}} kg</th>
+                                        </tr>
+                                        <tr>
+                                            <th class="my-th">CO emesso in un anno</th>
+                                            <th class="my-th">{{$this->pollutionArray['co']}} kg</th>
+                                        </tr>
+                                        <tr>
+                                            <th class="my-th">NOX emesso in un anno</th>
+                                            <th class="my-th">{{$this->pollutionArray['nox']}} kg</th>
+                                        </tr>
+                                        <tr>
+                                            <th class="my-th">PM10 emesse in un anno</th>
+                                            <th class="my-th">{{$this->pollutionArray['pm10']}} kg</th>
+                                        </tr>
+                                        <tr>
+                                            <th class="my-th">Alberi necessari per neutralizzare la CO2 emessa
+                                                annualmente
+                                            </th>
+                                            <th class="my-th">{{$this->pollutionArray['trees']}} alberi</th>
+                                        </tr>
+                                    @endif
                                     </tbody>
                                 </table>
                             </div>
