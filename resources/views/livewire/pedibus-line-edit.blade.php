e4375b10bffa4501d4f538567d02302d90f070f8
edobarto <edo99.barto@gmail.com>
2024-06-06 11:23:22 +0200

    feat: pedibus - gestione fermate

--- resources/views/livewire/pedibus-line-edit.blade.php
+++ resources/views/livewire/pedibus-line-edit.blade.php
@@ -1,7 +1,8 @@
 <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow">
     <div class="grid grid-cols-6 my-5 mx-auto gap-4 content-baseline">
         <div class="col-span-2">
-            <x-jet-input wire:model.lazy="line.name" class="w-full"/>
+            <h5 class="text-3xl font-bold">{{$line->name}}</h5>
+
         </div>
         <div class="col-span-1"></div>
         <div class="col-span-1">
@@ -25,7 +26,23 @@
             </x-jet-button>
         </div>
     </div>
-    <h5>Fermate</h5>
+    <h5 class="text-lg font-bold">Fermate</h5>
+
+    <div class="flex flex-row my-5 mx-auto gap-4 content-baseline">
+        <div  class="flex-1">
+                    <x-jet-label>Nome</x-jet-label>
+        </div>
+        <div class="flex-1">
+                    <x-jet-label>Indirizzo</x-jet-label>
+        </div>
+        <div class="flex-1">
+                    <x-jet-label>Ordine</x-jet-label>
+        </div>
+        <div >
+        </div>
+        <div>
+        </div>
+    </div>
 
     @foreach($this->pedibusStops as $stop)
         <livewire:pedibus-stop-edit wire:key="{{$stop->id}}" :pedibus-stop-id="$stop->id"></livewire:pedibus-stop-edit>
--- resources/views/livewire/pedibus-stop-edit.blade.php
+++ resources/views/livewire/pedibus-stop-edit.blade.php
@@ -1,28 +1,35 @@
-<div class="grid grid-cols-5 my-5 mx-auto gap-4 content-baseline">
-    <div class="col-span-1"></div>
-
-    <div class="col-span-1">
-        <x-jet-label>Nome</x-jet-label>
+<div class="flex flex-row my-5 mx-auto gap-4 content-baseline">
+    <div  class="flex-1">
+{{--        <x-jet-label>Nome</x-jet-label>--}}
 
         <x-jet-input wire:model.lazy="pedibusStop.name" class="w-full"/>
     </div>
-    <div class="col-span-1">
-        <x-jet-label>Indirizzo</x-jet-label>
+    <div class="flex-1">
+{{--        <x-jet-label>Indirizzo</x-jet-label>--}}
 
         <x-jet-input wire:model.lazy="pedibusStop.address" class="w-full"/>
     </div>
-    <div class="col-span-1">
-        <x-jet-label>Ordine</x-jet-label>
+    <div class="flex-1">
+{{--        <x-jet-label>Ordine</x-jet-label>--}}
         <x-select wire:model.lazy="pedibusStop.order" class="w-full">
             @for($count = 1; $count <= $this->pedibusStopsCount; $count++)
                 <option value="{{$count}}">{{$count}}</option>
             @endfor
         </x-select>
     </div>
-    <div class="col-span-1">
+    <div >
         <x-jet-button
+            class="mt-auto"
             wire:click="update"
         >Salva
+        </x-jet-button>
+    </div>
+    <div>
+        <x-jet-button
+            wire:click="update"
+        >
+            <i class="fa-solid mr-2 fa-location-dot"></i>
+            Disegna
         </x-jet-button>
     </div>
 </div>
